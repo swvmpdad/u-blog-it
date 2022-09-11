e763946750ce45247885cbae82f28229f7e02e06
swvmpdad <will.g.camp@gmail.com>
2022-09-11 17:16:36 -0500

    installed node packages, set up repo layout and added all requirements, server, and database connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to the database, pass in your MySQL information for username and password
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
